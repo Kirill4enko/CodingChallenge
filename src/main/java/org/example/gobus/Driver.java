a83ece4d5c6721aee2d463cb7e91c645f9a7472b
Kirill <kmukha2008@gmail.com>
2026-07-22 06:00:11 +0200

    Drivers

--- src/main/java/org/example/gobus/Driver.java
+++ src/main/java/org/example/gobus/Driver.java
@@ -2,6 +2,6 @@ package org.example.gobus;
 
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello Driver");
+        System.out.println("Hello Drivers!!!");
     }
 }
